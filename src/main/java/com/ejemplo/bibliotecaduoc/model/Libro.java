a63680016e3788a220975dfaa3d152a910c6ff4e
IartS <mat.durand@duocuc.cl>
2026-03-24 14:19:25 -0300

    Clase libro completa

--- src/main/java/com/ejemplo/bibliotecaduoc/model/Libro.java
+++ src/main/java/com/ejemplo/bibliotecaduoc/model/Libro.java
@@ -1,5 +1,9 @@
 package com.ejemplo.bibliotecaduoc.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor

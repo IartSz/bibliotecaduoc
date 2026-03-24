954020da7bb57d1c8f667318e4c5c63b68636c0a
IartS <mat.durand@duocuc.cl>
2026-03-24 14:14:27 -0300

    Clase libro completa

---
+++ src/main/java/com/ejemplo/bibliotecaduoc/model/Libro.java
@@ -0,0 +1,16 @@
+package com.ejemplo.bibliotecaduoc.model;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Libro {
+    private int id;
+    private String isbn;
+    private String titulo;
+    private String editorial;
+    private int fechaPublicacion;
+    private String autor;
+
+
+}
